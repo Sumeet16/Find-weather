28e85d34b26e85495a42ba1205c33439c87dcf24
Sumeet Yadav <77012823+Sumeet16@users.noreply.github.com>
2021-07-20 00:01:46 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const search = document.querySelector("svg");
 const viewMoreBox = document.querySelector(".info-card");
 const time = document.querySelector(".time");
 const date = document.querySelector(".date");
+const viewLess = document.querySelector(".less");
 
 let currTime = () => {
     let currDate = new Date();
@@ -30,9 +31,21 @@ setInterval(currTime, 1000);
 view.addEventListener("click", ()=>{
     viewSvg.classList.toggle("rotate");
     viewMoreBox.classList.toggle("visible");
+    view.classList.toggle("less");
+    if(view.classList.contains("less")){
+        view.innerHTML = "View Less";
+    }else{
+        view.innerHTML = "View More";
+    }
 });
 
 viewSvg.addEventListener("click", ()=>{
     viewSvg.classList.toggle("rotate");
     viewMoreBox.classList.toggle("visible");
+    view.classList.toggle("less");
+    if(view.classList.contains("less")){
+        view.innerHTML = "View Less";
+    } else{
+        view.innerHTML = "View More";
+    }
 });
